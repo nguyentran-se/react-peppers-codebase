319e2426555fbfdae55e2a1003839c97547e644b
nguyentran-se <nguyentranfw@gmail.com>
2021-07-14 07:06:19 +0700

    done setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PublicLayout from "./hoc/PublicLayout/PublicLayout";
+
+const App = () => {
+   return (
+      <PublicLayout>
+         <div>
+            <h1>[APP]Peppers mp3</h1>
+         </div>
+      </PublicLayout>
+   );
+};
+
+export default App;
